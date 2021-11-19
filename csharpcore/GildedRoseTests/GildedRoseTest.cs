d2a94f1521a4751c943ab038a3069f6c91fe4fde
tgdraugr <tiagogabrielsilva0@gmail.com>
2021-11-19 17:47:36 +0000

    Added test to cover the quality degradation after date as passed

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -17,5 +17,20 @@ namespace GildedRoseTests
             app.UpdateQuality();
             Assert.Equal(0, items[0].Quality);
         }
+
+        [Fact]
+        public void Should_degrade_quality_twice_as_fast_once_sell_date_has_passed()
+        {
+            var items = new List<Item>
+            {
+                new Item { Name = "an item ", SellIn = 0, Quality = 2 },
+                new Item { Name = "another item", SellIn = -1, Quality = 2 }
+            };
+
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(0, items[0].Quality);
+            Assert.Equal(0, items[1].Quality);
+        }
     }
 }
