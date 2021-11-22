6a29d6e8f6725c6d62b1c63522961f64c7dc3d56
tgdraugr <tiagogabrielsilva0@gmail.com>
2021-11-22 10:35:16 +0000

    Extracted some atomic functions to simplify code.

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -5,7 +5,7 @@ namespace GildedRoseKata
     public class GildedRose
     {
         private readonly IList<Item> _items;
-        
+
         public GildedRose(IList<Item> items)
         {
             _items = items;
@@ -15,75 +15,78 @@ namespace GildedRoseKata
         {
             foreach (var item in _items)
             {
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                UpdateQuality(item);
+            }
+        }
+
+        private static void UpdateQuality(Item item)
+        {
+            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    if (item.Quality > 0)
+                    DecreaseQuality(item);
+                }
+            }
+            else
+            {
+                IncreaseQuality(item);
+                
+                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (item.SellIn < 11)
+                    {
+                        IncreaseQuality(item);
+                    }
+
+                    if (item.SellIn < 6)
+                    {
+                        IncreaseQuality(item);
+                    }
+                }
+            }
+
+            if (item.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                item.SellIn -= 1;
+            }
+
+            if (item.SellIn < 0)
+            {
+                if (item.Name != "Aged Brie")
+                {
+                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
                         if (item.Name != "Sulfuras, Hand of Ragnaros")
                         {
-                            item.Quality -= 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality += 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality += 1;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality += 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.SellIn -= 1;
-                }
-
-                if (item.SellIn < 0)
-                {
-                    if (item.Name != "Aged Brie")
-                    {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.Quality > 0)
-                            {
-                                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    item.Quality -= 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            item.Quality -= item.Quality;
+                            DecreaseQuality(item);
                         }
                     }
                     else
                     {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality += 1;
-                        }
+                        item.Quality -= item.Quality;
                     }
                 }
+                else
+                {
+                    IncreaseQuality(item);
+                }
+            }
+        }
+
+        private static void DecreaseQuality(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality -= 1;
+            }
+        }
+
+        private static void IncreaseQuality(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality += 1;
             }
         }
     }
